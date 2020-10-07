a6d22be4830b406f5bcc3b8d198fb1a4929cf5db
squashetonics <70538586+squashetonics@users.noreply.github.com>
2020-10-07 17:58:24 +0200

    Initial Master Commit

---
+++ precache-manifest.2b39e3af3a1e2fec1fd5f9475f808ea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274b0a5d7b9448de67241ef3b300d519",
+    "url": "/Drum-Machine/index.html"
+  },
+  {
+    "revision": "6bcd8600c2885b2dfdff",
+    "url": "/Drum-Machine/static/css/main.2133c49b.chunk.css"
+  },
+  {
+    "revision": "6c61f01ca4bf3ca90048",
+    "url": "/Drum-Machine/static/js/2.5ca405df.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Drum-Machine/static/js/2.5ca405df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bcd8600c2885b2dfdff",
+    "url": "/Drum-Machine/static/js/main.16cb757e.chunk.js"
+  },
+  {
+    "revision": "c6dd1430e2afb8c8d750",
+    "url": "/Drum-Machine/static/js/runtime-main.d322cb89.js"
+  }
+]);
