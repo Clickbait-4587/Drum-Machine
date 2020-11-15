dccb61919a495364538f10a11073910f54443142
squashetonics <70538586+squashetonics@users.noreply.github.com>
2020-11-15 13:23:31 +0200

    Added Keydown eventlistener

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -11,25 +11,51 @@ class UI {
             padStyle: activeStyle,
         });
     };
-    playSound = () => {
-        let padClip = this.props.id;
-        console.log(padClip);
-        $("#display").text(padClip);
-        const sound = document.getElementById(this.props.keyTrigger);
-        sound.currentTime = 0;
-        try {
-            sound.play();
-        } catch (error) {
-            console.log(error);
-        }
+    static playSound = (e) => {
+        console.log(e);
+        if (e.type == "click") {
+            let aud = $(e.target).parent().find("audio")[0];
+            $(e.target).css({
+                border: "1px solid aliceblue",
+                position: "relative",
+                bottom: "4px",
+                background: "#a39f9f",
+            });
 
-        this.activatePad();
-        setTimeout(() => this.deactivatePad(), 100);
+            setTimeout(() => {
+                $(e.target).css({
+                    border: "none",
+                    background: "#616161",
+                    position: "relative",
+                    bottom: "0px",
+                });
+            }, 100);
+            aud.currentTime = 0;
+            $("#display").text(e.target.id);
+
+            aud.play();
+            if (aud.currentTime == aud.duration) {
+                aud.pause();
+                aud.currentTime = 0;
+            }
+        }
+        if (e.type == "keydown") {
+            let buttons = document.querySelectorAll("button");
+
+            buttons.forEach((btn) => {
+                if (btn.dataset.key == e.key.toUpperCase()) {
+                    btn.click();
+                }
+            });
+        }
     };
 }
 
 $(document).ready(function() {
     //Play audio
+    document.body.addEventListener("keydown", (e) => {
+        UI.playSound(e);
+    });
     let auds = document.querySelectorAll("audio");
     auds.forEach((aud) => {
         aud.oncanplay = (e) => {
@@ -37,10 +63,7 @@ $(document).ready(function() {
         };
     });
     $(".play-btn").click((e) => {
-        let aud = $(e.target).parent().find("audio")[0];
-        aud.currentTime = 0;
-        $("#display").text(e.target.id);
-        aud.play();
+        UI.playSound(e);
     });
 
     window.onload = (e) => {
